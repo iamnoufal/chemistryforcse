5b5b64f25fb93bef349decd7a5c033725444902b
Rahman24 <50796694+Rahman24@users.noreply.github.com>
2021-01-29 21:21:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
@@ -9,3 +9,31 @@ if ('serviceWorker' in navigator) {
     });
   });
 }
+
+let deferredPrompt;
+var ath=document.querySelector('.ath');
+ath.style.display="none";
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  ath.style.display="block";
+  // Update UI notify the user they can install the PWA
+  
+  ath.addEventListener('click', (e) => {
+  // Hide the app provided install promotion
+  ath.style.display="none";
+  // Show the install prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
+    }
+  });
+});
+});
