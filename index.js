a94d873de6d19a8e909e9d34558048c87362e555
Rahman24 <50796694+Rahman24@users.noreply.github.com>
2021-02-06 17:17:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+function offline () {
+  alert("Looks like you are offline.....Try again after sometime");
+}
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('sw.js').then(function(registration) {
